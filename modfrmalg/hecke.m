fbd66cc060b8b28c7f66906cb18514ecfb690d3b
Eran Assaf <assaferan@gmail.com>
2021-04-14 15:27:37 -0400

    Update hecke.m
    refactoring, removing code duplicates

--- modfrmalg/hecke.m
+++ modfrmalg/hecke.m
@@ -97,160 +97,146 @@ declare attributes ModHecke:
 
 // set methods
 
+// We use this function for several intrinsic interfaces below
+procedure internalSetHecke(~M, T, pR, k)
+  // If assocative array does not exist for this dimension, create one.
+  if not IsDefined(M`Hecke`Ts, k) then
+    M`Hecke`Ts[k] := AssociativeArray();
+  end if;
+
+  // Assign Hecke matrix.
+  M`Hecke`Ts[k][pR] := T;
+
+  // Assign Hecke images of standard basis vectors
+  if not assigned M`Hecke`standard_images then
+    M`Hecke`standard_images :=
+      [AssociativeArray() : i in [1..Dimension(M)]];
+  end if;
+  for i in [1..Dimension(M)] do
+    if not IsDefined(M`Hecke`standard_images[i], k) then
+      M`Hecke`standard_images[i][k] := AssociativeArray();
+    end if;
+    M`Hecke`standard_images[i][k][pR] := Transpose(T)[i];
+  end for;
+end procedure;
+
 intrinsic SetHeckeOperator(
 	M::ModFrmAlg, T::AlgMatElt, pR::RngOrdIdl, k::RngIntElt)
 { Sets the k-th order Hecke operator at the specified prime for this form. }
-	// If assocative array does not exist for this dimension, create one.
-	if not IsDefined(M`Hecke`Ts, k) then
-		M`Hecke`Ts[k] := AssociativeArray();
-	end if;
-
-	// Assign Hecke matrix.
-        M`Hecke`Ts[k][pR] := T;
-
-	// Assign Hecke images of standard basis vectors
-	if not assigned M`Hecke`standard_images then
-	    M`Hecke`standard_images :=
-		[AssociativeArray() : i in [1..Dimension(M)]];
-	end if;
-	for i in [1..Dimension(M)] do
-	    if not IsDefined(M`Hecke`standard_images[i], k) then
-		M`Hecke`standard_images[i][k] := AssociativeArray();
-	    end if;
-	    M`Hecke`standard_images[i][k][pR] := Transpose(T)[i];
-	end for;
+  internalSetHecke(~M, T, pR, k);
 end intrinsic;
 
 intrinsic SetHeckeOperator(M::ModFrmAlg, T::AlgMatElt, pR:RngOrdIdl)
 { Sets the 1-st order Hecke operator at the specified prime for this form. }
-	SetHeckeOperator(M, T, pR, 1);
+  SetHeckeOperator(M, T, pR, 1);
 end intrinsic;
 
 intrinsic SetHeckeOperator(
 	M::ModFrmAlg, T::AlgMatElt, pR::RngInt, k::RngIntElt)
 { Sets the k-th order Hecke operator at the specified prime for this form. }
-	// If assocative array does not exist for this dimension, create one.
-	if not IsDefined(M`Hecke`Ts, k) then
-		M`Hecke`Ts[k] := AssociativeArray();
-	end if;
-
-	// Assign Hecke matrix.
-        M`Hecke`Ts[k][pR] := T;
-
-	// Assign Hecke images of standard basis vectors
-	if not assigned M`Hecke`standard_images then
-	    M`Hecke`standard_images :=
-		[AssociativeArray() : i in [1..Dimension(M)]];
-	end if;
-	for i in [1..Dimension(M)] do
-	    if not IsDefined(M`Hecke`standard_images[i], k) then
-		M`Hecke`standard_images[i][k] := AssociativeArray();
-	    end if;
-	    M`Hecke`standard_images[i][k][pR] := Transpose(T)[i];
-	end for;
+  internalSetHecke(~M, T, pR, k);
 end intrinsic;
 
 intrinsic SetHeckeOperator(M::ModFrmAlg, T::AlgMatElt, pR::RngInt)
 { Sets the 1-st order Hecke operator at the specified prime for this form. }
-	SetHeckeOperator(M, T, pR, 1);
+  SetHeckeOperator(M, T, pR, 1);
 end intrinsic;
 
 intrinsic SetHeckeOperator(
 	M::ModFrmAlg, T::AlgMatElt, p::RngIntElt, k::RngIntElt)
 { Sets the k-th order Hecke operator at the specified prime for this form. }
-	SetHeckeOperator(M, T, ideal< Integers() | p >, k);
+  SetHeckeOperator(M, T, ideal< Integers() | p >, k);
 end intrinsic;
 
 intrinsic SetHeckeOperator(M::ModFrmAlg, T::AlgMatElt, p::RngIntElt)
 { Sets the 1-st order Hecke operator at the specified prime for this form. }
-	SetHeckeOperator(M, T, p, 1);
+  SetHeckeOperator(M, T, p, 1);
 end intrinsic;
 
 // get (computation) methods
+
+// internal function for several of the interfaces below
+function internalHecke(M, pR, k, BeCareful, Force, Estimate, UseLLL, Orbits,
+		       Genus, LowMemory)
+  // Initialize the space of algebraic modular forms, if needed.
+  if not Genus then
+    ModFrmAlgInit(M : BeCareful := BeCareful,
+		  Orbits := Orbits, LowMemory := LowMemory);
+  end if;
+
+  // If the orders don't agree, check to see if their field of fractions
+  //  are isomorphic. If so, convert the ideal passed as an argument to
+  //  an ideal in terms of the number ring given by the appropriate ring.
+  if Order(pR) ne BaseRing(Module(M)) then
+	    
+    // Assign fields of fractions.
+    K1 := FieldOfFractions(Order(pR));
+    K2 := FieldOfFractions(BaseRing(Module(M)));
+	    
+     // Check for isomorphism.
+     isom, map := IsIsomorphic(K1, K2);
+
+     // If they are isomorphic, reassign the provided prime ideal.
+     error if not isom, "The number field of the prime ideal should be isomorphic to the field of definition of M.\n";
+
+     // Assign the new prime ideal.
+     pR := ideal< BaseRing(Module(M))
+             | [ map(x) : x in Generators(pR) ] >;
+  end if;
+
+  // Look for the requested Hecke operator.
+  ok, Ts := IsDefined(M`Hecke`Ts, k);
+  if ok and not Force then
+    ok, T := IsDefined(Ts, pR);
+    if ok then return T; end if;
+  end if;
+
+  // Choose the appropriate routine for computing Hecke operators.
+  // Right now, in this version, we do not choose, but use always the same
+  // function.
+
+  // Currently LLL doesn't seem to work for SO
+  if UseLLL and not IsSpecialOrthogonal(M) then
+    use_LLL := true;
+  else
+   use_LLL := false;
+  end if;
+
+  if Genus then
+    hecke := [];
+    HeckeOperatorAndGenusCN1(~M, pR, k, ~hecke : BeCareful := BeCareful,
+			     UseLLL := use_LLL, Estimate := Estimate,
+			     Orbits := Orbits);
+  elif Orbits and LowMemory then
+    hecke := HeckeOperatorCN1OrbitLowMemory(M, pR, k : BeCareful := BeCareful,
+				      UseLLL := use_LLL, Estimate := Estimate);
+  else
+    hecke := HeckeOperatorCN1(M, pR, k : BeCareful := BeCareful,
+			      UseLLL := use_LLL, Estimate := Estimate,
+			      Orbits := Orbits);
+  end if;
+
+  // Sets the Hecke operator in the internal data structure for this
+  //  algebraic modular form.
+  SetHeckeOperator(M, hecke, pR, k);
+
+  // Returns the computed Hecke operator.
+  return hecke;
+end function;
 
 intrinsic HeckeOperator(M::ModFrmAlg, pR::RngOrdIdl, k::RngIntElt
 			: BeCareful := false,
 			  Force := false,
 			  Estimate := true,
 			  UseLLL := true,
-			  Fast := false,
 			  Orbits := true,
 			  Genus := false,
 			  LowMemory := false) -> AlgMatElt
 { Computes the requested Hecke operator. }
-	// Initialize the space of algebraic modular forms, if needed.
-        if not Genus then
-	  ModFrmAlgInit(M : BeCareful := BeCareful,
-			Orbits := Orbits, LowMemory := LowMemory);
-        end if;
-
-	// Verify that the supplied ideal is prime.
-	require IsPrime(pR): "Provided ideal must be prime.";
-
-	// If the orders don't agree, check to see if their field of fractions
-	//  are isomorphic. If so, convert the ideal passed as an argument to
-	//  an ideal in terms of the number ring given by the appropriate ring.
-	if Order(pR) ne BaseRing(Module(M)) then
-	    
-	    // Assign fields of fractions.
-	    K1 := FieldOfFractions(Order(pR));
-	    K2 := FieldOfFractions(BaseRing(Module(M)));
-	    
-	    // Check for isomorphism.
-	    isom, map := IsIsomorphic(K1, K2);
-
-	    // If they are isomorphic, reassign the provided prime ideal.
-	    require isom: "Incompatible base rings.";
-
-	    // Assign the new prime ideal.
-	    pR := ideal< BaseRing(Module(M))
-		       | [ map(x) : x in Generators(pR) ] >;
-	end if;
-
-	// Look for the requested Hecke operator.
-	ok, Ts := IsDefined(M`Hecke`Ts, k);
-	if ok and not Force then
-		ok, T := IsDefined(Ts, pR);
-		if ok then return T; end if;
-	end if;
-
-	// Choose the appropriate routine for computing Hecke operators.
-	// Right now, in this version, we do not choose, but use always the same
-	// function.
-
-	// Currently LLL doesn't seem to work for SO
-	if UseLLL and not IsSpecialOrthogonal(M) then
-	    use_LLL := true;
-	else
-	    use_LLL := false;
-	end if;
-
-        if Genus then
-	  hecke := [];
-	  HeckeOperatorAndGenusCN1(~M, pR, k, ~hecke
-				  : BeCareful := BeCareful,
-				    UseLLL := use_LLL,
-				    Estimate := Estimate,
-				    Orbits := Orbits);
-        elif Orbits and LowMemory then
-	  hecke := HeckeOperatorCN1OrbitLowMemory(M, pR, k
-				    : BeCareful := BeCareful,
-				      UseLLL := use_LLL,
-				      Estimate := Estimate);
-	else
-	  hecke := HeckeOperatorCN1(M, pR, k
-				    : BeCareful := BeCareful,
-				      UseLLL := use_LLL,
-				      Estimate := Estimate,
-				      Orbits := Orbits);
-	end if;
-
-	// Sets the Hecke operator in the internal data structure for this
-	//  algebraic modular form.
-	SetHeckeOperator(M, hecke, pR, k);
-
-	// Returns the computed Hecke operator.
-	return hecke;
+  // Verify that the supplied ideal is prime.
+  require IsPrime(pR): "Provided ideal must be prime.";
+  return internalHecke(M, pR, k, BeCareful, Force, Estimate, UseLLL, Orbits,
+		       Genus, LowMemory);
 end intrinsic;
 
 intrinsic HeckeOperator(M::ModFrmAlg, pR::RngOrdIdl
@@ -258,7 +244,6 @@ intrinsic HeckeOperator(M::ModFrmAlg, pR::RngOrdIdl
 			  Force := false,
 			  Estimate := false,
 			  UseLLL := true,
-			  Fast := false,
 			  Orbits := true,
 			  LowMemory := false,
 			  Genus := false) -> AlgMatElt
@@ -268,7 +253,6 @@ intrinsic HeckeOperator(M::ModFrmAlg, pR::RngOrdIdl
 			       Force := Force,
 			       Estimate := Estimate,
 			       UseLLL := UseLLL,
-			       Fast := Fast,
 			       Orbits := Orbits,
 			       LowMemory := LowMemory,
 			       Genus := Genus);
@@ -279,85 +263,15 @@ intrinsic HeckeOperator(M::ModFrmAlg, pR::RngInt, k::RngIntElt
 			  Force := false,
 			  Estimate := false,
 			  UseLLL := true,
-			  Fast := false,
 			  Orbits := false,
 			  LowMemory := false,
 			  Genus := false) -> AlgMatElt
 { Computes the requested Hecke operator, under the assumption that the base
 number field is the rationals. }
-	// Initialize the space of algebraic modular forms, if needed.
-        if not Genus then
-	  ModFrmAlgInit(M : BeCareful := BeCareful,
-			Orbits := Orbits, LowMemory := LowMemory);
-        end if;
-
-	// Verify that the supplied ideal is prime.
-	require IsPrime(pR): "Provided ideal must be prime.";
-
-	// If the orders don't agree, check to see if their field of fractions
-	//  are isomorphic. If so, convert the ideal passed as an argument to
-	//  an ideal in terms of the number ring given by the appropriate ring.
-	if Order(pR) ne BaseRing(Module(M)) then
-	    
-	    // Assign fields of fractions.
-	    K1 := FieldOfFractions(Order(pR));
-	    K2 := FieldOfFractions(BaseRing(Module(M)));
-	    
-	    // Check for isomorphism.
-	    isom, map := IsIsomorphic(K1, K2);
-
-	    // If they are isomorphic, reassign the provided prime ideal.
-	    require isom: "Incompatible base rings.";
-
-	    // Assign the new prime ideal.
-	    pR := ideal< BaseRing(Module(M))
-		       | [ map(x) : x in Generators(pR) ] >;
-	end if;
-
-	// Look for the requested Hecke operator.
-	ok, Ts := IsDefined(M`Hecke`Ts, k);
-	if ok and not Force then
-		ok, T := IsDefined(Ts, pR);
-		if ok then return T; end if;
-	end if;
-
-	// Choose the appropriate routine for computing Hecke operators.
-	// Right now, in this version, we do not choose, but use always the same
-	// function.
-
-	// Currently LLL doesn't seem to work for SO
-	if UseLLL and not IsSpecialOrthogonal(M) then
-	    use_LLL := true;
-	else
-	    use_LLL := false;
-	end if;
-
-        if Genus then
-	  hecke := [];
-	  HeckeOperatorAndGenusCN1(~M, pR, k, ~hecke
-				  : BeCareful := BeCareful,
-				    UseLLL := use_LLL,
-				    Estimate := Estimate,
-				    Orbits := Orbits);
-        elif Orbits and LowMemory then
-	  hecke := HeckeOperatorCN1OrbitLowMemory(M, pR, k
-				    : BeCareful := BeCareful,
-				      UseLLL := use_LLL,
-				      Estimate := Estimate);
-	else
-	  hecke := HeckeOperatorCN1(M, pR, k
-				    : BeCareful := BeCareful,
-				      UseLLL := use_LLL,
-				      Estimate := Estimate,
-				      Orbits := Orbits);
-	end if;
-
-	// Sets the Hecke operator in the internal data structure for this
-	//  algebraic modular form.
-	SetHeckeOperator(M, hecke, pR, k);
-
-	// Returns the computed Hecke operator.
-	return hecke;
+  // Verify that the supplied ideal is prime.
+  require IsPrime(pR): "Provided ideal must be prime.";
+  return internalHecke(M, pR, k, BeCareful, Force, Estimate, UseLLL, Orbits,
+		       Genus, LowMemory);
 end intrinsic;
 
 intrinsic HeckeOperator(M::ModFrmAlg, pR::RngInt
@@ -365,7 +279,6 @@ intrinsic HeckeOperator(M::ModFrmAlg, pR::RngInt
 			  Force := false,
 			  Estimate := false,
 			  UseLLL := true,
-			  Fast := false,
 			  Orbits := true,
 			  LowMemory := false,
 			  Genus := false) -> AlgMatElt
@@ -377,7 +290,6 @@ intrinsic HeckeOperator(M::ModFrmAlg, pR::RngInt
 			       Force := Force,
 			       Estimate := Estimate,
 			       UseLLL := UseLLL,
-			       Fast := Fast,
 			       Orbits := Orbits,
 			       LowMemory := LowMemory,
 			       Genus := Genus);
@@ -388,7 +300,6 @@ intrinsic HeckeOperator(M::ModFrmAlg, p::RngIntElt, k::RngIntElt
 			  Force := false,
 			  Estimate := true,
 			  UseLLL := false,
-			  Fast := false,
 			  Orbits := true,
 			  LowMemory := false,
 			  Genus := false) -> AlgMatElt
@@ -400,7 +311,6 @@ number field is the rationals. }
 			       Force := Force,
 			       Estimate := Estimate,
 			       UseLLL := UseLLL,
-			       Fast := Fast,
 			       Orbits := Orbits,
 			       LowMemory := LowMemory,
 			       Genus := Genus);
@@ -411,7 +321,6 @@ intrinsic HeckeOperator(M::ModFrmAlg, p::RngIntElt
 			  Force := false,
 			  Estimate := true,
 			  UseLLL := false,
-			  Fast := false,
 			  Orbits := true,
 			  LowMemory := false,
 			  Genus := false) -> AlgMatElt
@@ -422,7 +331,6 @@ assumption that the base number field is the rationals. }
 			       Force := Force,
 			       Estimate := Estimate,
 			       UseLLL := UseLLL,
-			       Fast := Fast,
 			       Orbits := Orbits,
 			       LowMemory := LowMemory,
 			       Genus := Genus);
@@ -466,17 +374,9 @@ function SparseRepresentation(M, v)
    return sp;
 end function;
 
-intrinsic HeckeImages(M::ModFrmAlg, i::RngIntElt,
-				       n::RngIntElt, k::RngIntElt :
-		      BeCareful := false,
-		      Estimate := true,
-		      Orbits := true,
-		      UseLLL := false,
-		      LowMemory := false) -> SeqEnum
-{The images of the ith standard basis vector
- under the Hecke operators Tp^k for p good prime, such that Norm(p)<=n
-These are computed using sparse methods that don't
-require computing the full Hecke operator.}  
+// This will be used for both versions of HeckeImages
+function internalHeckeImages(M, i, prec, k, BeCareful,
+			     Estimate, Orbits, UseLLL, LowMemory)
    assert 1 le i and i le Dimension(M);
    if not assigned M`Hecke`standard_images then
        M`Hecke`standard_images :=
@@ -501,11 +401,15 @@ require computing the full Hecke operator.}
 	       Norm(p)) eq 1 and IsSplit(p)];
   */
    // bad_modulus := Numerator(Norm(Discriminant(Module(M))));
-   R := Integers(BaseRing(M));
-   if Type(R) eq RngInt then
-     ps :=  PrimesUpTo(n, Rationals());
+   if Type(prec) eq RngIntElt then
+     R := Integers(BaseRing(M));
+     if Type(R) eq RngInt then
+       ps :=  PrimesUpTo(prec, Rationals());
+     else
+       ps := [Factorization(R!!p)[1][1] : p in PrimesUpTo(prec, Rationals())];
+     end if;
    else
-     ps := [Factorization(R!!p)[1][1] : p in PrimesUpTo(n, Rationals())];
+     ps := prec;
    end if;
    if SpaceType(AmbientSpace(Module(M))) eq "Hermitian" then
        alpha := Involution(ReflexiveSpace(Module(M)));
@@ -526,7 +430,22 @@ require computing the full Hecke operator.}
 	       Transpose(sp_mat)[j-start_idx+1];
        end for;
    end for;
-   return M`Hecke`standard_images[i][k];       
+   return M`Hecke`standard_images[i][k];  
+end function;
+
+intrinsic HeckeImages(M::ModFrmAlg, i::RngIntElt,
+				       n::RngIntElt, k::RngIntElt :
+		      BeCareful := false,
+		      Estimate := true,
+		      Orbits := true,
+		      UseLLL := false,
+		      LowMemory := false) -> SeqEnum
+{The images of the ith standard basis vector
+ under the Hecke operators Tp^k for p good prime, such that Norm(p)<=n
+These are computed using sparse methods that don't
+require computing the full Hecke operator.}  
+  return internalHeckeImages(M, i, n, k, BeCareful,
+			     Estimate, Orbits, UseLLL, LowMemory);     
 end intrinsic;
 
 
@@ -541,36 +460,6 @@ intrinsic HeckeImages(M::ModFrmAlg, i::RngIntElt,
  under the Hecke operators Tp^k for p good prime, such that Norm(p)<=n
 These are computed using sparse methods that don't
 require computing the full Hecke operator.}  
-   assert 1 le i and i le Dimension(M);
-   if not assigned M`Hecke`standard_images then
-       M`Hecke`standard_images :=
-	   [AssociativeArray() : j in [1..Dimension(M)]];
-   end if;
-   s := SparseRepresentation(M, VectorSpace(M).i);
-   space_idx := s[1][2];
-   start_idx := s[1][3];
-   end_idx := start_idx + Dimension(M`H[space_idx]) - 1;
-   assert start_idx le i and i le end_idx;
-   // Due to the nature of the computation, we compute an entire block together
-   if not IsDefined(M`Hecke`standard_images[i], k) then
-       for j in [start_idx..end_idx] do
-	   M`Hecke`standard_images[j][k] := AssociativeArray();
-       end for;
-   end if;
-   
-   new_ps := [p : p in ps | p notin Keys(M`Hecke`standard_images[i][k])];
-   for p in new_ps do
-       sp_hec := HeckeOperatorCN1Sparse(M, p, k, s : BeCareful := BeCareful,
-						     Estimate := Estimate,
-						     Orbits := Orbits,
-					             UseLLL := UseLLL,
-					             LowMemory := LowMemory);
-       sp_mat := sp_hec[space_idx];
-       for j in [start_idx..end_idx] do
-	   M`Hecke`standard_images[j][k][p] :=
-	       Transpose(sp_mat)[j-start_idx+1];
-       end for;
-   end for;
-
-   return M`Hecke`standard_images[i][k];       
+  return internalHeckeImages(M, i, ps, k, BeCareful,
+		      Estimate, Orbits, UseLLL, LowMemory);   
 end intrinsic;

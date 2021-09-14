c9a136b0515d2945d8d06f2e921882c88d855beb
assaferan <assaferan@gmail.com>
2021-09-14 17:00:02 -0400

    magma-mode files

--- neighbors/.magma-scan/hecke-CN1.m.el
+++ neighbors/.magma-scan/hecke-CN1.m.el
@@ -1,3 +1,3 @@
 ;;; This file was generated automatically.
 
-(setq magma-completion-table (-union magma-completion-table '("HeckeOperatorCN1Sparse" "HeckeOperatorCN1SparseBasis" "processNeighborWeightSparse" "HeckeOperatorCN1" "processNeighborWeight" "printEstimate" )))
+(setq magma-completion-table (-union magma-completion-table '("HeckeOperatorCN1Sparse" "HeckeOperatorCN1SparseBasis" "HeckeOperatorCN1" "finalizeHecke" "HeckeOperatorCN1Update" "createReducedInvs2" "createReducedInvs" "createInvsWithPrecision" "processNeighborWeight" )))

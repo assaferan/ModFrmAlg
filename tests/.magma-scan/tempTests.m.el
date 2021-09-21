b3123b459430dbc2ec24773a1c72930b9c8c326e
assaferan <assaferan@gmail.com>
2021-09-21 12:42:48 -0400

    modified path

--- tests/.magma-scan/tempTests.m.el
+++ tests/.magma-scan/tempTests.m.el
@@ -1,3 +1,3 @@
 ;;; This file was generated automatically.
 
-(setq magma-completion-table (-union magma-completion-table '("otherStuff" "identifyMaximalSubgroup" "swap" "getThirdElation" "findConjugation" "updateConjugation" "groupCentralElations" "getMaximalSubgroup" "invariantSubspace" "centralElationPointOfFixedPlanes" "getGaloisImages" "getGaloisImage" "inspect" )))
+(setq magma-completion-table (-union magma-completion-table '("otherStuff" "inspect" )))

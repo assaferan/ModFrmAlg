0330cde9676facb42f21aa3607714edb41083a31
assaferan <assaferan@gmail.com>
2021-09-21 08:29:05 -0400

    magma-mode files

--- lattice/.magma-scan/lattice.m.el
+++ lattice/.magma-scan/lattice.m.el
@@ -1,3 +1,3 @@
 ;;; This file was generated automatically.
 
-(setq magma-completion-table (-union magma-completion-table '("get_hecke_representatives" "pAdicLatticeAtSplitPrime" "GuessMaxDet" "SolveCongruence2" "CanLiftSquare" "SolveCongruence" "MyDiagonal" "GramMatrixOfBasis" "pMaximalGram" )))
+(setq magma-completion-table (-union magma-completion-table '("ElementsWithSupport" "MySquarefree" "get_hecke_representatives" "pAdicLatticeAtSplitPrime" "is_maximal_integral" "GuessMaxDet" "SolveCongruence2" "CanLiftSquare" "SolveCongruence" "MyDiagonal" "GramMatrixOfBasis" "pMaximalGram" )))

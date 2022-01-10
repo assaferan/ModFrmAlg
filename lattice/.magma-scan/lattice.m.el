dcee063f9340354295f5cc7670deda8fe8461aec
assaferan <55987323+assaferan@users.noreply.github.com>
2022-01-10 08:24:46 -0500

    Delete lattice/.magma-scan directory

--- lattice/.magma-scan/lattice.m.el
+++
@@ -1,3 +0,0 @@
-;;; This file was generated automatically.
-
-(setq magma-completion-table (-union magma-completion-table '("ElementsWithSupport" "MySquarefree" "get_hecke_representatives" "pAdicLatticeAtSplitPrime" "is_maximal_integral" "GuessMaxDet" "SolveCongruence2" "CanLiftSquare" "SolveCongruence" "MyDiagonal" "GramMatrixOfBasis" "pMaximalGram" )))

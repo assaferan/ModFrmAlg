1268e530843bd715631086084dfddc31512f2620
assaferan <55987323+assaferan@users.noreply.github.com>
2022-01-10 08:25:19 -0500

    Delete neighbors/.magma-scan directory

--- neighbors/.magma-scan/genus-CN1.m.el
+++
@@ -1,3 +0,0 @@
-;;; This file was generated automatically.
-
-(setq magma-completion-table (-union magma-completion-table '("OrthogonalMass" "Combine" "LocalFactor" "WittToHasse" "UnitaryMass" "DedekindZetaExact" "myEval" "ExtensionToHeckeCharacter" "ClassGroupPrimeIdealGenerators" "CGPrimes" "sortGenusCN1" "computeGenusRepsCN1" "computeGenusRepsAt" "checkNextNeighbor" )))
--- neighbors/.magma-scan/hecke-CN1.m.el
+++
@@ -1,3 +0,0 @@
-;;; This file was generated automatically.
-
-(setq magma-completion-table (-union magma-completion-table '("HeckeOperatorCN1Sparse" "HeckeOperatorCN1SparseBasis" "HeckeOperatorCN1" "finalizeHecke" "HeckeOperatorCN1Update" "createReducedInvs2" "createReducedInvs" "createInvsWithPrecision" "processNeighborWeight" )))

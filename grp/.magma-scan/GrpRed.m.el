7a6a831fc2641e7ea03958be4121556ff7b7fbb2
assaferan <55987323+assaferan@users.noreply.github.com>
2022-01-10 08:27:07 -0500

    Delete grp/.magma-scan directory

--- grp/.magma-scan/GrpRed.m.el
+++
@@ -1,3 +0,0 @@
-;;; This file was generated automatically.
-
-(setq magma-completion-table (-union magma-completion-table '("extract_root_datum" "build_root_data" "SplitReflexiveSpace" "build_orthogonal" )))

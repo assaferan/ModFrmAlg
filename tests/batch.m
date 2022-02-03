a172811912e7bc4b228d6423a6b451853987a63e
assaferan <assaferan@gmail.com>
2022-02-03 14:12:42 -0500

    Update batch.m

--- tests/batch.m
+++ tests/batch.m
@@ -380,12 +380,12 @@ function createHeckeBatchFile(omf_name, pR, k, pivot_idx, start, upTo, hecke_idx
   f := Open(batch_fname, "w");
   output_str := "GetSeed();\n";
   output_str cat:= "AttachSpec(\"ModFrmAlg.spec\");\n";
-  output_str cat:= Sprintf("M := AlgebraicModularForms(\"%o\");\n", omf_name cat ".omf"));
-  output_str cat:= Sprintf("pR := %m;\n",pR));
-  output_str cat:= Sprintf("nProc := InitPivots(M, pR, %o, %o);\n", k, hecke_idx));
+  output_str cat:= Sprintf("M := AlgebraicModularForms(\"%o\");\n", omf_name cat ".omf");
+  output_str cat:= Sprintf("pR := %m;\n",pR);
+  output_str cat:= Sprintf("nProc := InitPivots(M, pR, %o, %o);\n", k, hecke_idx);
   output_str cat:= Sprintf("hecke := HeckePivot(M, nProc, %o, %o, %o, %o, %o);\n",
-			    pivot_idx, ThetaPrec, hecke_idx, start, upTo));
-  output_str cat:= Sprintf("Write(\"%o\", Eltseq(hecke));\n", output_fname));
+			    pivot_idx, ThetaPrec, hecke_idx, start, upTo);
+  output_str cat:= Sprintf("Write(\"%o\", Eltseq(hecke));\n", output_fname);
   output_str cat:= "exit;\n";
   fprintf f, output_str;
   delete f;

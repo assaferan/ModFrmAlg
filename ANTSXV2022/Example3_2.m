af265c259a5b3c28310950905e0686a8c7c71a78
assaferan <assaferan@gmail.com>
2022-03-03 20:54:17 -0500

    Added Example 4.10
    Also added some comments for the other ones.

--- ANTSXV2022/Example3_2.m
+++ ANTSXV2022/Example3_2.m
@@ -1,7 +1,14 @@
+// This is example 3.2 from the paper.
 AttachSpec("../ModFrmAlg.spec");
 Q := SymmetricMatrix([2,0,2,0,1,34,1,0,0,34]);
 M := OrthogonalModularForms(Q);
 time Dimension(M);
+/*
+13
+Time: 0.300
+*/
 time fs := HeckeEigenforms(M);
-
+/*
+Time: 0.170
+*/
 exit;
--- ANTSXV2022/Example4_9.m
+++ ANTSXV2022/Example4_9.m
@@ -1,4 +1,4 @@
-// This is a continuation of Example 3.1.
+// This is Example 4.9 from the paper, a continuation of Example 3.1.
 AttachSpec("../ModFrmAlg.spec");
 // A gram matrix representing the genus 
 Q := SymmetricMatrix([2,0,4,1,1,10,1,2,1,20]);

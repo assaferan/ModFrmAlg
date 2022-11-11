dd8e5500ff6c20af97d9d7b155ce0ea2aac4a2a4
Eran Assaf <assaferan@gmail.com>
2022-11-10 21:28:47 -0500

    fixing JV name

--- lattice/minkowski.m
+++ lattice/minkowski.m
@@ -3,7 +3,7 @@ freeze;
                                                                             
                         Algebraic Modular Forms in Magma
                         
-                  E. Assaf, M. Greenberg, J. Hein, J.Voight
+                  E. Assaf, M. Greenberg, J. Hein, J. Voight
          using lattices over number fields by M. Kirschmer and D. Lorch         
                    
    FILE: minkowski.m

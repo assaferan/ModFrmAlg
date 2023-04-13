516bebd58a414849d9a736b3d0a85b44c2e8eae8
assaferan <assaferan@gmail.com>
2023-04-13 10:13:02 -0400

    path for saving and loading

--- io/path.m
+++ io/path.m
@@ -6,5 +6,6 @@
 intrinsic GetAMFPath() -> MonStgElt
 {Returns the path for saving and loading files for algebraic modular forms.
 It is currently hard-coded, as I haven't figured out how to do it differently in magma.}
-  return "/Users/eranassaf/Documents/GitHub/ModFrmAlg/data/";
+  // return "/Users/eranassaf/Documents/GitHub/ModFrmAlg/data/";
+  return "./data/";
 end intrinsic;

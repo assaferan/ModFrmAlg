e8efaa50c54ceb9ff4985c66dcab967806377c0c
assaferan <assaferan@gmail.com>
2024-04-18 16:07:09 -0400

    adding files, still no changes

---
+++ verbose/version.m
@@ -0,0 +1,23 @@
+freeze;
+ 
+/****-*-magma-************************************************
+                                                                          
+                  VERSION: Algebraic Modular Forms in Magma
+
+ FILE: version.m (Version Support)                                        
+                                                                       
+ Last updated : 03/02/2024, Eran Assaf
+
+***************************************************************************/
+
+intrinsic CheckVersion()
+{.}
+  v1, v2, v3 := GetVersion();
+  version := Vector([v1, v2, v3]);
+
+  if version lt Vector([2,22,6]) then   
+      error "This package is not supported on Magma version ", 
+	    v1, ".", v2, "-", v3;
+  end if;
+  return;
+end intrinsic;

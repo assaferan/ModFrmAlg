cf22d45d0f199bc6387900c12e59fc5724784e31
assaferan <assaferan@gmail.com>
2024-06-06 10:18:57 -0400

    trying to figure out why discriminant test fails on github workers. added printouts

--- tests/Discriminant2.m
+++ tests/Discriminant2.m
@@ -1,10 +1,15 @@
 procedure testDiscriminant2()
-  printf "Testing John's question about discriminant 2...";
-  T := Matrix([[2,0,0,0,1],[0,2,0,0,1],[0,0,2,1,1],[0,0,1,2,0],[1,1,1,0,2]]);
-  L := LatticeWithGram(T);
-  Lambda := LatticeFromLat(L);
-  assert Discriminant(Lambda) eq FractionalIdeal(2);
-  assert Norm(Discriminant(Lambda)) eq 2;
+    printf "Testing John's question about discriminant 2...";
+    printf "creating matrix...";
+    T := Matrix([[2,0,0,0,1],[0,2,0,0,1],[0,0,2,1,1],[0,0,1,2,0],[1,1,1,0,2]]);
+    printf "creating lattice...";
+    L := LatticeWithGram(T);
+    printf "creating ModDedLat object from lattice...";
+    Lambda := LatticeFromLat(L);
+    printf "Checking discriminant...";
+    assert Discriminant(Lambda) eq FractionalIdeal(2);
+    printf "Checking norm of discriminant...";
+    assert Norm(Discriminant(Lambda)) eq 2;
 end procedure;
 
 testDiscriminant2();

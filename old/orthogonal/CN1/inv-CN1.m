9f441dd60a6b68d77f8c52031579825292da7da2
Eran Assaf <assaferan@gmail.com>
2024-10-09 20:14:05 -0400

    removing old irrelevant files

--- old/orthogonal/CN1/inv-CN1.m
+++
@@ -1,6 +0,0 @@
-// This script computes an invariant used to sort genus representatives.
-
-function Invariant(L)
-	return ThetaSeries(ZLattice(L), 25);
-end function;
-
--- old/orthogonal/kirschmer/lat.spec
+++
@@ -1,7 +0,0 @@
-{
-  lat.m
-  misc.m
-  sigfield.m
-  genus.m
-  unimodular.m
-}
